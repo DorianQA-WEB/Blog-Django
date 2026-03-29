0a21c73989f6a6081d8ae6452c67ad18597c845f
DorianQA-WEB <qa.doriangrey@gmail.com>
2026-03-29 21:20:51 +0500

    Страницы

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
+from django.core.paginator import Paginator
 
 def post_list(request):
-    posts = Post.objects.all()
+    all_posts = Post.objects.all()
+    # Постраничная разбивка с 3 постами на страницу
+    paginator = Paginator(all_posts, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request,
-                  'blog/post/list.html', {'posts': posts})
+                  'blog/post/list.html',
+                  {'posts': posts})
 
 
-def post_detail(request, post_id):
+def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Post,
-                             id=post_id,
-                             status=Post.Status.PUBLISHED)
+                             status=Post.Status.PUBLISHED,
+                             slug=slug,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
