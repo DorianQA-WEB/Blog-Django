be25603decf11bc5712931f860116e4f0d69661e
DorianQA-WEB <qa.doriangrey@gmail.com>
2026-04-01 17:44:46 +0500

    share

--- blog/views.py
+++ blog/views.py
@@ -54,6 +54,7 @@ def post_share(request, post_id):
     post = get_object_or_404(Post,
                              id=post_id,
                              status=Post.Status.PUBLISHED)
+    sent = False
     if request.method == 'POST':
     # Форма была передана на обработку
         form = EmailPostForm(request.POST)
@@ -62,7 +63,7 @@ def post_share(request, post_id):
             cd = form.cleaned_data
             post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = f'{cd['name']} recommends you read {post.title}'
-            message = f'Read {post.title} at {post_url}\n\n {cd['name']}\'s ({cd['email']}) comments: {cd['comments']}
+            message = f'Read {post.title} at {post_url}\n\n {cd['name']}\'s ({cd['email']}) comments: {cd['comments']}'
             send_mail(subject, message, settings.EMAIL_HOST_USER, [cd['to']])
             sent = True
             # ... отправить электронное письмо
