4c19b1736e6b5bb71009318677aa7c88997b7c98
DorianQA-WEB <qa.doriangrey@gmail.com>
2026-04-02 21:35:24 +0500

    search

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.conf import settings
 from django.views.decorators.http import require_POST
+
 from taggit.models import Tag
 from django.db.models import Count
-
-
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 
 # class PostListView(ListView):
 #     """
@@ -107,3 +108,24 @@ def post_comment(request, post_id):
         comment.save()
     return render(request, 'blog/post/comment.html',
                   {'post': post, 'form': form, 'comment': comment})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
