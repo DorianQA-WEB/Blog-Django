9fa01e1565eb7ab6733e3c88aa50845a9abe7b0b
DorianQA-WEB <qa.doriangrey@gmail.com>
2026-04-03 19:32:39 +0500

    logout

---
+++ accounts/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+    username = forms.CharField(max_length=30)
+    email = forms.EmailField(max_length=200)
+    password1 = forms.CharField(max_length=50, widget=forms.PasswordInput())
+    password2 = forms.CharField(max_length=50, widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput(attrs={'placeholder': "Username"}))
+    password = forms.CharField(max_length=50,
+                               required=True,
+                               widget=forms.PasswordInput(attrs={'placeholder': "Password"}))
+    remember_me = forms.BooleanField(required=False)
+
