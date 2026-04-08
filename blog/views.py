b0b4a33dd642521e6928d2170e47e198b041dad4
DorianQA-WEB <qa.doriangrey@gmail.com>
2026-04-08 21:40:44 +0500

    bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.conf import settings
@@ -9,7 +8,6 @@ from django.views.decorators.http import require_POST
 
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.contrib.postgres.search import TrigramSimilarity
 
 # class PostListView(ListView):
